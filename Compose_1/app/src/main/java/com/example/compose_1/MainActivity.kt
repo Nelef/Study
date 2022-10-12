97b2867b8bd228259f8f47adb1e39f3d6fbb119e
uyjang <imoneleft@naver.com>
2022-10-12 16:34:48 +0900

    no message

--- Compose_1/app/src/main/java/com/example/compose_1/MainActivity.kt
+++ Compose_1/app/src/main/java/com/example/compose_1/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.compose_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_1.ui.theme.Compose_1Theme
 
 class MainActivity : ComponentActivity() {
@@ -51,7 +54,14 @@ fun Greeting(name: String = "test") {
 @Preview
 @Composable
 fun Card_test() {
-    Card {
-        Text(text = "Simple Card")
+    Card(
+        shape = RoundedCornerShape(13.dp),
+        backgroundColor = Color.LightGray, modifier = Modifier.padding(16.dp)
+    ) {
+        Column {
+            Text(text = "Simple Card", modifier = Modifier.padding(16.dp))
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Compose", modifier = Modifier.padding(16.dp))
+        }
     }
 }
