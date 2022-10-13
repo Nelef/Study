7d2ecf0481fb47968b0bcb3146e9ed836abcb794
uyjang <imoneleft@naver.com>
2022-10-13 13:59:03 +0900

    COMPOSE Scaffold2

--- Compose_1/app/src/main/java/com/example/compose_1/MainActivity.kt
+++ Compose_1/app/src/main/java/com/example/compose_1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.compose_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -10,6 +12,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,48 +23,70 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Compose_1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    DefaultPreview()
-                }
-            }
+//            Compose_1Theme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+//                ) {
+//                    DefaultPreview()
+//                }
+//            }
+            DefaultPreview()
         }
     }
 }
 
-@Preview(name = "Hello", showBackground = true)
+@Preview(name = "Preview_title", showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Compose_1Theme {
+    Column {
         Greeting("Android")
-        Column {
-            Greeting("Android")
-            Card_test()
-        }
+        Card_test()
+        BoxExample()
     }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun Greeting(name: String = "test") {
     Text(text = "Hello $name!")
 }
 
-@Preview
 @Composable
 fun Card_test() {
     Card(
-        shape = RoundedCornerShape(13.dp),
-        backgroundColor = Color.LightGray, modifier = Modifier.padding(16.dp)
+        elevation = 10.dp, shape = RoundedCornerShape(13.dp),
+        backgroundColor = Color.LightGray, modifier = Modifier.padding(16.dp),
     ) {
         Column {
             Text(text = "Simple Card", modifier = Modifier.padding(16.dp))
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = "Compose", modifier = Modifier.padding(16.dp))
         }
+    }
+}
+
+@Composable
+fun BoxExample() {
+    Box(
+        modifier = Modifier
+            .background(color = Color.Cyan)
+            .size(150.dp, 200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = Color.Black)
+                .size(50.dp, 100.dp)
+                .align(Alignment.TopEnd)
+        )
+        Text(
+            text = "Hello",
+            style = MaterialTheme.typography.h5,
+            modifier = Modifier
+                .background(color = Color.White)
+                .padding(10.dp)
+                .align(
+                    Alignment.BottomCenter
+                )
+        )
     }
 }
--- MovieApp/app/src/main/java/com/example/movieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,28 +18,45 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp() {
+                MainContent()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Test() {
+    MyApp {
+        MainContent()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MyApp(content: @Composable () -> Unit) {
     MovieAppTheme {
-        Greeting("Android")
+        Scaffold( topBar = {
+            TopAppBar(
+                title = { Text(text = "Title text") },
+                navigationIcon = {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
+                },
+                actions = {
+                    Icon(imageVector = Icons.Default.Favorite, contentDescription = "Favorite")
+                    Icon( imageVector = Icons.Default.Search, contentDescription = "Search")
+                }
+            )
+        },) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    // A surface container using the 'background' color from the theme
+    Surface(color = MaterialTheme.colors.background) {
+        Text(text = "Hello")
     }
 }
